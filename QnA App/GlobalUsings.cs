67fe3c81e435c52dc588e49eb6bba729aaf6c87f
Kiran Awale <Kiran.Awale@wolterskluwer.com>
2023-12-15 23:46:05 +0530

    Add project files.

---
+++ QnA App/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System;
+global using System.IO;
+global using System.Text;
+global using Microsoft.Extensions.Configuration;
+global using Azure;
+global using Azure.AI.Language.QuestionAnswering;
---
+++ QnA App/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace QnA_App
+{
+    public class Program
+    {
+        protected Program() { }
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                // Get config settings from AppSettings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string aiSvcEndpoint = configuration["AIServicesEndpoint"];
+                string aiSvcKey = configuration["AIServicesKey"];
+                string projectName = configuration["QAProjectName"];
+                string deploymentName = configuration["QADeploymentName"];
+
+                // Create client using endpoint and key
+                AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
+                Uri endpoint = new Uri(aiSvcEndpoint);
+                QuestionAnsweringClient aiClient = new QuestionAnsweringClient(endpoint, credentials);
+
+                // Submit a question and display the answer
+                string user_question = "";
+
+                while (user_question.ToLower() != "quit") {
+                    Console.WriteLine("Question: ");
+                    user_question = Console.ReadLine();
+                    QuestionAnsweringProject project = new QuestionAnsweringProject(projectName, deploymentName);
+                    Response<AnswersResult> response = await aiClient.GetAnswersAsync(user_question,project);
+
+                    foreach (KnowledgeBaseAnswer answer in response.Value.Answers)
+                    {
+                        await Console.Out.WriteLineAsync(answer.Answer);
+                        await Console.Out.WriteLineAsync($"Confidence: {answer.Confidence:P2}");
+                        await Console.Out.WriteLineAsync($"Source: {answer.Source}");
+                        await Console.Out.WriteLineAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
